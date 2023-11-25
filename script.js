6f22ac27de435928344fdafbe13115116a727309
Pratham Kamble <prathamkamble727@gmail.com>
2023-11-25 23:59:08 +0530

    done

--- script.js
+++ script.js
@@ -1,68 +1,61 @@
-let users = [];
+const submitBtn = document.getElementById("submitBtn");
 
-// Function to generate a random 16-byte token
-function generateToken() {
-    return [...Array(10)].map(() => Math.floor(Math.random() * 256).toString(10).padStart(2, '0')).join('');
-}
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 
-// Function to handle form submission
-function submitForm() {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
 
-    // Display error message if any field is empty
-    const genericError = document.getElementById('genericError');
+  const genericError = document.getElementById('genericError');
 
-    if (!name || !email || !password || !confirmPassword) {
-        genericError.style.display = 'block';
-        return;
-    }
+  if (!name || !email || !password || !confirmPassword) {
+    genericError.style.display = 'block';
+    return;
+  }
+  if (password !== confirmPassword) {
+    alert("Passwords do not match!");
+    return;
+  } else console.log("password matched");
 
-    // Reset the generic error message
-    genericError.style.display = 'none';
+  const token = generateAccessToken();
+  console.log('token no: ' + token);
 
-    if (password !== confirmPassword) {
-        alert("Passwords do not match!");
-        return;
-    }
+  // Store user details in local storage
+  let userData = {
+    name: name,
+    email: email,
+    token: token,
+    password: password,
+  };
 
-    // Simulated backend action: save user data
-    const token = generateToken();
-    users.push({ name, email, token, password });
+  localStorage.setItem("userData", JSON.stringify(userData));
+  console.log(`user Data : ${userData}`);
 
-    // Redirect to profile page and pass user data
-    localStorage.setItem('currentUser', JSON.stringify({ name, email, token, password }));
+//   redirect to page after few seconds
+  setTimeout(function () {
     window.location.href = './profile.html';
+  }, 1000);
+});
+
+// token generation fn
+function generateAccessToken(){
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const tokenArray = [];
+
+  for (let i = 0; i < 16; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    tokenArray.push(charset[randomIndex]);
+  }
+
+  return tokenArray.join("");
 }
-
-
-const currentUser = JSON.parse(localStorage.getItem('currentUser'));
-const currentPage = window.location.pathname;
-
-if (!currentUser && currentPage !== '/index.html') {
-    // Redirect to the signup page only if there's no user data and it's not already the signup page
-    window.location.href = './index.html';
-} else if (currentUser) {
-    document.addEventListener('DOMContentLoaded', function () {
-        const profileName = document.getElementById('profileName');
-        const profileEmail = document.getElementById('profileEmail');
-        const profileToken = document.getElementById('profileToken');
-        const profilePassword = document.getElementById('profilePassword');
-
-        if (profileName && profileEmail && profileToken) {
-            profileName.innerText = currentUser.name;
-            profileEmail.innerText = currentUser.email;
-            profileToken.innerText = currentUser.token;
-            profilePassword.innerText = currentUser.password;
+    window.onload = function () {
+        // This function will be executed after the page has loaded
+        // alert("on signUp page")
+        if (localStorage.getItem("userData")) {
+          window.location.href = "./profile.html";
+          alert("Already Loggedin");
         }
-    });
-}
-
-
-// Function to handle logout
-function logout() {
-    localStorage.removeItem('currentUser');
-    window.location.href = 'index.html'; // Redirect to signup page after logout
-}
+    };
